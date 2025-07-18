8df4f3ccbd382bf43b6c6483c83e81b6536083b2
ExceptedPrism3 <ben.ayoube@outlook.com>
2025-07-18 16:25:52 +0200

    4.1 // Code Cleanup + Auto Join After Restart

---
+++ utils/database.js
@@ -0,0 +1,35 @@
+const Database = require('better-sqlite3');
+const path = require('path');
+
+const dbPath = path.resolve(__dirname, '../data/channels.db');
+const db = new Database(dbPath);
+
+db.pragma('journal_mode = WAL');
+
+db.exec(`
+  CREATE TABLE IF NOT EXISTS channels (
+    guildId TEXT PRIMARY KEY,
+    channelId TEXT NOT NULL
+  )
+`);
+
+function saveChannel(guildId, channelId) {
+  const stmt = db.prepare('INSERT OR REPLACE INTO channels (guildId, channelId) VALUES (?, ?)');
+  stmt.run(guildId, channelId);
+}
+
+function removeChannel(guildId) {
+  const stmt = db.prepare('DELETE FROM channels WHERE guildId = ?');
+  stmt.run(guildId);
+}
+
+function getAllChannels() {
+  const stmt = db.prepare('SELECT * FROM channels');
+  return stmt.all();
+}
+
+module.exports = {
+  saveChannel,
+  removeChannel,
+  getAllChannels
+};
